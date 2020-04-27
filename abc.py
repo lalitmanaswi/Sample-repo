441132667d16954528e19d865614ee1ae330d4db
Lalit Manaswi <lalitmanaswi@gmail.com>
2020-04-27 12:21:05 +0530

    Removed some code

--- abc.py
+++ abc.py
@@ -1,14 +1,6 @@
-import sys
-
-print(sys.version)
-print(sys.executable)
-
-
 def who_to_greet(greet):
     greeting = "Hello, {}".format(greet)
     return greeting
 
-
 print(who_to_greet("World"))
-print(who_to_greet("Lalit"))
 
